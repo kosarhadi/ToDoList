02f9c20aad5f08108fac76b76b336c8bc5f1fa46
kosar <kosarhadiabedini@gmail.com>
2023-02-25 11:03:18 +0330

    user don

--- Models/user.py
+++ Models/user.py
@@ -1,11 +1,18 @@
+import itertools
 class User:
     count=0
-    def __init__(self,first_name,last_name,password,email_address):
-        self.first_name=first_name
-        self.last_name=last_name
+    id_obj=itertools.count()
+    def __init__(self,usar_name,password):
+        self.id=next(User.id_obj)
+        self.usar_name=usar_name
         self.password=password
-        self.email_address=email_address
 
         User.count+=1
+    
+    def __del__(self):
+
+        User.count-=1
+
+        
 
     
