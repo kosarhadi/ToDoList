8d3aa857a30b9065a10ad0fd8b613a346c7c3645
kosar <kosarhadiabedini@gmail.com>
2023-02-25 11:43:58 +0330

    task don

---
+++ Models/task.py
@@ -0,0 +1,20 @@
+import itertools
+class Task:
+    count=0
+    id_obj=itertools.count()
+    def __init__(self,title,status,date,userid):
+        self.id=next(Task.id_obj)
+        self.title=title
+        self.status=status
+        self.date=date
+        self.userid=userid
+
+        Task.count+=1
+    
+    def __del__(self):
+
+        Task.count-=1
+
+        
+
+    
